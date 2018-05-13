d07660a1bda1567883e5303fca3c4c7c14c8c90c
Thomas <thomas@wondersauce.com>
2018-05-13 09:49:17 -0400

    keeping this

--- src/proof/Proof.js
+++ src/proof/Proof.js
@@ -4,6 +4,9 @@
 import React, { Component } from 'react';
 import './Proof.css';
 import sha256 from 'crypto-js/sha256';
+import chainpointClient from 'chainpoint-client';
+// import chainpointClient from 'chainpoint-client-js';
+
 
 class Proof extends Component {
 	state = {
@@ -11,24 +14,26 @@ class Proof extends Component {
 		org_public_key: '',
 		timestamp: '',
 		proof_id: '',
-		hash: ''
+		hash: '',
+		proof: ''
 	};
 	render() {
 		const props = this.state;
 		return (
 			<div>
 				<div className="form-wrapper">
-					<form className="step-2-form" onSubmit={this.submit}>
+					<form className="step-2-form" onSubmit={this.sendHash}>
 						<input type="text" name="client_public_key" placeholder="Public key" value={props.client_public_key}  onChange={this.handleChange} />
 						<input type="text" name="org_public_key" placeholder="Organization key" value={props.org_public_key}  onChange={this.handleChange} />
-						<input type="date" name="timestamp" value={props.timestamp}  onChange={this.handleChange} />
+						<input type="date" name="timestamp" value={props.timestamp} onChange={this.handleChange} />
 						{/*<input type="date" name="firstName" placeholder="First Name" value={Time.now()} onChange={this.handleChange} />*/}
 						<button type="submit">Submit</button>
 					</form>
 				</div>
 				<div className="info-wrapper">
 					<p className="proof-info">
-						Hash : {props.hash}
+						Hash : {this.state.hash}
+						Proof : {this.state.proof}
 					</p>
 				</div>
 			</div>
@@ -45,10 +50,43 @@ class Proof extends Component {
 	};
 	
 	generateHash = () => {
-	 let hash = this.state.client_public_key + this.state.org_public_key + this.state.timestamp.toString();
-   const new_hash = sha256(hash).toString();
-	 this.setState({hash: new_hash});
+	 // let date = this.state.timestamp.toLocaleDateString();
+	 let proof = this.state.client_public_key + " " +  this.state.org_public_key + " " + this.state.timestamp.toString();
+   const new_hash = sha256(proof).toString();
+		
+		this.setState({hash: new_hash,proof: proof});
+	}
+	sendHash = () => {
+		chainpointClient.submitHashes(this.state.hash);
 	}
 }
 
 export default Proof;
+
+
+// async function runIt (hash) {
+// 	// A few sample SHA-256 proofs to anchor
+// 	// let hashes = ['1d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a',
+// 	// 	'2d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a',
+// 	// 	'3d2a9e92b561440e8d27a21eed114f7018105db00262af7d7087f7dea9986b0a']
+// 	let hashes = [hash];
+//
+// 	// Submit each hash to three randomly selected Nodes
+// 	let proofHandles = await chp.submitHashes(hashes)
+// 	console.log("Submitted Proof Objects: Expand objects below to inspect.")
+// 	console.log(proofHandles)
+//
+// 	// Wait for Calendar proofs to be available
+// 	console.log("Sleeping 12 seconds to wait for proofs to generate...")
+// 	await new Promise(resolve => setTimeout(resolve, 12000))
+//
+// 	// Retrieve a Calendar proof for each hash that was submitted
+// 	let proofs = await chp.getProofs(proofHandles)
+// 	console.log("Proof Objects: Expand objects below to inspect.")
+// 	console.log(proofs)
+//
+// 	// Verify every anchor in every Calendar proof
+// 	let verifiedProofs = await chp.verifyProofs(proofs)
+// 	console.log("Verified Proof Objects: Expand objects below to inspect.")
+// 	console.log(verifiedProofs)
+// }
